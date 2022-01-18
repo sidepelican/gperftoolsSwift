fa737032f6f54832377b304c4249a5ae4b1ce3df
Iceman <side.junktown@gmail.com>
2022-01-18 19:22:23 +0900

    add binary download path

--- SampleApp/Sources/Run/main.swift
+++ SampleApp/Sources/Run/main.swift
@@ -4,18 +4,33 @@ import Vapor
 
 let app = Application()
 
+let profilePath = NSTemporaryDirectory() + "a.profile"
+
 app.get("profile") { (req) -> EventLoopFuture<Response> in
-    let result = NSTemporaryDirectory() + "a.profile"
     let waitSecond = min((try? req.query.get(TimeInterval.self, at: "s")) ?? 30, 120)
-    Profiler.start(fname: result)
+    Profiler.start(fname: profilePath)
 
     let promise = req.eventLoop.makePromise(of: Response.self)
     DispatchQueue.global().asyncAfter(deadline: .now() + waitSecond) {
         Profiler.stop()
-        let res = req.fileio.streamFile(at: result)
+        let res = req.fileio.streamFile(at: profilePath)
         promise.completeWith(.success(res))
     }
     return promise.futureResult
+}
+
+app.get("profile_start") { (req) -> String in
+    Profiler.start(fname: profilePath)
+    return "start!"
+}
+
+app.get("profile_end") { (req) -> Response in
+    Profiler.stop()
+    return req.fileio.streamFile(at: profilePath)
+}
+
+app.get("binary") { (req) -> Response in
+    return req.fileio.streamFile(at: ProcessInfo.processInfo.arguments[0])
 }
 
 try app.register(collection: MyController())
